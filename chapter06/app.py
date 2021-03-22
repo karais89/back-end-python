3f76bd233c5af7ddc8ce71db0b26f062551896bf
kaya <kaya@treenod.com>
2021-03-22 23:43:28 +0900

    6장. singn-up and tweet

--- chapter06/app.py
+++ chapter06/app.py
@@ -1,99 +1,73 @@
-from flask import Flask, jsonify, request
-from flask.json import JSONEncoder
-
-app = Flask(__name__)
-app.users = {}  # 새로 가입한 사용자 users란 변수에 정의 한다.
-app.id_count = 1  # 회원 가입하는 사용자의 id 값을 저장
-app.tweets = []
+from flask import Flask, jsonify, request, current_app
+from sqlalchemy import create_engine, text
 
 
-class CustomJSONEncoder(JSONEncoder):
-    def default(self, obj):
-        if isinstance(obj, set):
-            return list(obj)
+# create_app이라는 함수 정의. Flask가 create_app이라는 함수를 자동으로 팩토리 함수로 인식함
+def create_app(test_config=None):
+    app = Flask(__name__)
 
-        return JSONEncoder.default(self, obj)
+    if test_config is None:
+        app.config.from_pyfile("config.py")
+    else:
+        app.config.update(test_config)
 
+    database = create_engine(app.config['DB_URL'], encoding='utf-8', max_overflow=0)
+    app.database = database
 
-app.json_encoder = CustomJSONEncoder
+    # 회원가입 엔드포인트
+    @app.route("/sign-up", methods=['POST'])
+    def sign_up():
+        new_user = request.json
+        new_user_id = current_app.database.execute(text("""
+        INSERT INTO users (
+            name,
+            email,
+            profile,
+            hashed_password
+        ) VALUES (
+            :name,
+            :email,
+            :profile,
+            :password
+        )
+        """), new_user).lastrowid
 
+        row = current_app.database.execute(text("""
+            SELECT id, name, email, profile
+            FROM users
+            WHERE id = :user_id
+        """), {
+            'user_id': new_user_id
+        }).fetchone()
 
-@app.route("/ping", methods=['GET'])
-def ping():
-    return "pong"
+        # 파이썬 식 3항 연산자.. (참인경우 값 if 조건 else 거짓인경우 값)
+        created_user = {
+            'id': row['id'],
+            'name': row['name'],
+            'email': row['email'],
+            'profile': row['profile']
+        } if row else None
 
+        return jsonify(created_user)
 
-@app.route("/sign-up", methods=['POST'])
-def sign_up():
-    new_user = request.json  # json -> dictionary
-    new_user["id"] = app.id_count
-    app.users[app.id_count] = new_user
-    app.id_count = app.id_count + 1
+    @app.route('/tweet', methods=['POST'])
+    def tweet():
+        user_tweet = request.json
+        tweet = user_tweet['tweet']
 
-    return jsonify(new_user)  # dictionary -> json
+        if len(tweet) > 300:
+            return '300자를 초과했습니다', 400
 
+        current_app.database.execute(text("""
+            INSERT INTO tweets (
+                user_id,
+                tweet
+            ) VALUES (
+                :id,
+                :tweet
+            )
+        """), user_tweet)
 
-@app.route('/tweet', methods=['POST'])
-def tweet():
-    payload = request.json
-    user_id = int(payload['id'])
-    tweet = payload['tweet']
+        return '', 200
 
-    if user_id not in app.users:
-        return '사용자가 존재하지 않습니다', 400
-
-    if len(tweet) > 300:
-        return '300자를 초과했습니다', 400
-
-    user_id = int(payload['id'])
-    app.tweets.append({
-        'user_id': user_id,
-        'tweet': tweet
-    })
-
-    return '', 200
-
-
-@app.route('/follow', methods=['POST'])
-def follow():
-    payload = request.json
-    user_id = int(payload['id'])
-    user_id_to_follow = int(payload['follow'])
-
-    if user_id not in app.users or user_id_to_follow not in app.users:
-        return '사용자가 존재하지 않습니다', 400
-
-    user = app.users[user_id]
-    user.setdefault('follow', set()).add(user_id_to_follow)  # setdefault dictionary 기능
-
-    return jsonify(user)
-
-
-@app.route('/unfollow', methods=['POST'])
-def unfollow():
-    payload = request.json
-    user_id = int(payload['id'])
-    user_id_to_follow = int(payload['unfollow'])
-
-    if user_id not in app.users or user_id_to_follow not in app.users:
-        return '사용자가 존재하지 않습니다', 400
-
-    user = app.users[user_id]
-    user.setdefault('follow', set()).add(user_id_to_follow)  # setdefault dictionary 기능
-
-    return jsonify(user)
-
-
-@app.route('/timeline/<int:user_id>', methods=['GET'])  # 엔드포인트 주소 <int:user_id> /timeline/1 같은 형태에서 1이 user_id에 저장됨
-def timeline(user_id):
-    if user_id not in app.users:
-        return '사용자가 존재하지 않습니다', 400
-
-    follow_list = app.users[user_id].get('follow', set())
-    follow_list.add(user_id)
-    timeline = [tweet for tweet in app.tweets if tweet['user_id'] in follow_list]
-
-    return jsonify({
-        'user_id': user_id,
-        'timeline': timeline
-    })
+    return app
