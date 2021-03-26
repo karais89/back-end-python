74794fc89f75a42289f96e8ff451567a290aba15
kaya <kaya@treenod.com>
2021-03-27 06:41:04 +0900

    6장. 리팩토링

--- chapter06/app.py
+++ chapter06/app.py
@@ -1,10 +1,108 @@
 from flask import Flask, jsonify, request, current_app
+from flask.json import JSONEncoder
 from sqlalchemy import create_engine, text
+
+
+# Defalut JSON encoder는 set을 JSON으로 변환할 수 없다.
+# 그러므로 커스텀 인코더를 작성하여 set을 list로 변환하여
+# JSON으로 변환 가능하게 해주어야 한다.
+class CustomJSONEncoder(JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, set):
+            return list(obj)
+
+        return JSONEncoder.default(self, obj)
+
+
+def get_user(user_id):
+    user = current_app.database.execute(text("""
+            SELECT id, name, email, profile
+            FROM users
+            WHERE id = :user_id
+        """), {
+        'user_id': user_id
+    }).fetchone()
+
+    return {
+        'id': user['id'],
+        'name': user['name'],
+        'email': user['email'],
+        'profile': user['profile']
+    } if user else None
+
+
+def insert_user(user):
+    return current_app.database.execute(text("""
+    INSERT INTO users (
+        name,
+        email,
+        profile,
+        hashed_password
+    ) VALUES (
+        :name,
+        :email,
+        :profile,
+        :password
+    )
+    """), user).lastrowid
+
+
+def insert_tweet(user_tweet):
+    return current_app.database.execute(text("""
+        INSERT INTO tweets (
+            user_id,
+            tweet
+        ) VALUES (
+            :id,
+            :tweet
+        )
+    """), user_tweet).rowcount
+
+
+def insert_follow(user_follow):
+    return current_app.database.execute(text("""
+            INSERT INTO users_follow_list (
+                user_id,
+                follow_user_id
+            ) VALUES (
+                :id,
+                :follow
+            )
+            """), user_follow).rowcount
+
+
+def insert_unfollow(user_unfollow):
+    return current_app.database.execute(text("""
+                DELETE FROM users_follow_list 
+                WHERE user_id = :id 
+                AND follow_user_id = :unfollow
+                """), user_unfollow).rowcount
+
+
+def get_timeline(user_id):
+    timeline = current_app.database.execute(text("""
+        SELECT 
+            t.user_id,
+            t.tweet
+        FROM tweets t
+        LEFT JOIN users_follow_list ufl ON ufl.user_id = :user_id
+        WHERE t.user_id = :user_id
+        OR t.user_id = ufl.follow_user_id
+    """), {
+        'user_id': user_id
+    }).fetchall()
+
+    return [{
+        'user_id': tweet['user_id'],
+        'tweet': tweet['tweet']
+    } for tweet in timeline]
 
 
 # create_app이라는 함수 정의. Flask가 create_app이라는 함수를 자동으로 팩토리 함수로 인식함
 def create_app(test_config=None):
     app = Flask(__name__)
+
+    app.json_encoder = CustomJSONEncoder
 
     if test_config is None:
         app.config.from_pyfile("config.py")
@@ -23,37 +121,11 @@ def create_app(test_config=None):
     @app.route("/sign-up", methods=['POST'])
     def sign_up():
         new_user = request.json
-        new_user_id = current_app.database.execute(text("""
-        INSERT INTO users (
-            name,
-            email,
-            profile,
-            hashed_password
-        ) VALUES (
-            :name,
-            :email,
-            :profile,
-            :password
-        )
-        """), new_user).lastrowid
+        new_user_id = insert_user(new_user)
+        new_user = get_user(new_user_id)
 
-        row = current_app.database.execute(text("""
-            SELECT id, name, email, profile
-            FROM users
-            WHERE id = :user_id
-        """), {
-            'user_id': new_user_id
-        }).fetchone()
-
-        # 파이썬 식 3항 연산자.. (참인경우 값 if 조건 else 거짓인경우 값)
-        created_user = {
-            'id': row['id'],
-            'name': row['name'],
-            'email': row['email'],
-            'profile': row['profile']
-        } if row else None
-
-        return jsonify(created_user)
+        return jsonify(new_user)
+    
 
     @app.route('/tweet', methods=['POST'])
     def tweet():
@@ -63,63 +135,28 @@ def create_app(test_config=None):
         if len(tweet) > 300:
             return '300자를 초과했습니다', 400
 
-        current_app.database.execute(text("""
-            INSERT INTO tweets (
-                user_id,
-                tweet
-            ) VALUES (
-                :id,
-                :tweet
-            )
-        """), user_tweet)
+        insert_tweet(user_tweet)
 
         return '', 200
 
     @app.route('/timeline/<int:user_id>', methods=['GET'])
     def timeline(user_id):
-        rows = current_app.database.execute(text("""
-            SELECT 
-                t.user_id,
-                t.tweet
-            FROM tweets t
-            LEFT JOIN users_follow_list ufl ON ufl.user_id = :user_id
-            WHERE t.user_id = :user_id
-            OR t.user_id = ufl.follow_user_id
-        """), {
-            'user_id': user_id
-        }).fetchall()
-
-        timeline = [{
-            'user_id': row['user_id'],
-            'tweet': row['tweet']
-        } for row in rows]
-
         return jsonify({
             'user_id': user_id,
-            'timeline': timeline
+            'timeline': get_timeline(user_id)
         })
 
     @app.route('/follow', methods=['POST'])
     def follow():
         payload = request.json
-        rowcount = current_app.database.execute(text("""
-                INSERT INTO users_follow_list (
-                    user_id,
-                    follow_user_id
-                ) VALUES (
-                    :id,
-                    :follow
-                )
-                """), payload).rowcount
-
+        insert_follow(payload)
+        
         return '', 200
 
     @app.route('/unfollow', methods=['POST'])
     def unfollow():
         payload = request.json
-        rowcount = current_app.database.execute(text("""
-                        DELETE FROM users_follow_list WHERE user_id = :id AND follow_user_id = :unfollow
-                        """), payload).rowcount
+        insert_unfollow(payload)
 
         return '', 200
 
